f3ca8af7fc494db8f8346a92246e90a6506b5cd2
Bayk <admiquel94@gmail.com>
2013-04-22 22:18:26 +0200

    Activity which takes (and controls) the values given by the user to create a Profile in the Database #7

--- src/org/touchirc/view/CreateProfileActivity.java
+++ src/org/touchirc/view/CreateProfileActivity.java
@@ -1,24 +1,20 @@
 package org.touchirc.view;
 
 import org.touchirc.R;
-import org.touchirc.model.Message;
+import org.touchirc.db.Database;
 import org.touchirc.model.Profile;
 
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
-
 
 public class CreateProfileActivity extends Activity{
 
@@ -41,15 +37,12 @@ public class CreateProfileActivity extends Activity{
 	private EditText realName;
 
 	private Profile createdProfile;
-	
-	private LinearLayout ll;
-
 
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 
-		setContentView(R.layout.createprofile_layout);
-		
+		setContentView(R.layout.create_profile_layout);
+
 		// TextViews : black ones (default color)s are compulsories, gray ones are optionals
 
 		this.profileName_TV = (TextView) findViewById(R.id.textView_profile_name);	
@@ -61,11 +54,11 @@ public class CreateProfileActivity extends Activity{
 
 		this.thirdNickname_TV = (TextView) findViewById(R.id.textView_third_nick);
 		this.thirdNickname_TV.setTextColor(Color.GRAY);
-		
+
 		this.userName_TV = (TextView) findViewById(R.id.textView_username);
-		
+
 		this.realName_TV = (TextView) findViewById(R.id.textView_realname);
-		
+
 		// EditTexts		
 
 		this.profileName = (EditText) findViewById(R.id.editText_profile_name);
@@ -79,9 +72,7 @@ public class CreateProfileActivity extends Activity{
 		this.userName = (EditText) findViewById(R.id.editText_username);
 
 		this.realName = (EditText) findViewById(R.id.editText_realname);
-		
-		this.ll = (LinearLayout) findViewById(R.id.linearLayout);
-		
+
 		/**
 		 * Create an OnEditorActionListener Object :
 		 * When the Key "Done" is pressed on the Realname EditText : a Profile is created.
@@ -92,32 +83,38 @@ public class CreateProfileActivity extends Activity{
 			@Override
 			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
 				boolean handled = false;
+
 				// the keyboard disappears
 				InputMethodManager mngr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 				mngr.hideSoftInputFromWindow(realName.getWindowToken(), 0);
+
 				if (arg1 == EditorInfo.IME_ACTION_DONE && (
 						profileName.getText().length() != 0 && 
 						firstNickname.getText().length() != 0 &&
 						userName.getText().length() != 0 && 
 						realName.getText().length() != 0)) {
-					
-					// the Profile is created with the datas given by the user
-					createdProfile = new Profile(profileName.getText().toString(), firstNickname.getText().toString(), secondNickname.getText().toString(), thirdNickname.getText().toString(), userName.getText().toString(), realName.getText().toString());
 
-					
-					System.out.println("Profile Name = " + createdProfile.getProfile_name() + "\n1st Nick = " + createdProfile.getFirstNick() +
-							"\n2nd Nick = " + createdProfile.getSecondNick() + "\n3rd Nick = " + createdProfile.getThirdNick() + "\nUsername = " + 
-							createdProfile.getUsername() + "\nReal name = " + createdProfile.getRealname());
+					// the Profile is created with the datas given by the user
+					createdProfile = new Profile(profileName.getText().toString(),
+							firstNickname.getText().toString(),
+							secondNickname.getText().toString(),
+							thirdNickname.getText().toString(),
+							userName.getText().toString(),
+							realName.getText().toString());
+
+					// Add the profile just created into the database
+					Database db = new Database(getApplicationContext());
+					db.addProfile(createdProfile);
 
 					handled = true;
-					
+
 					Toast.makeText(getApplicationContext(), "The profile : " + createdProfile.getProfile_name() + "has been added !", Toast.LENGTH_SHORT).show();
 				}
 				else{
 					// We alarm the user of missing informations
 					Toast.makeText(getApplicationContext(), "Missing informations ...", Toast.LENGTH_SHORT).show();
-					
-					// And indicate him of which informations we are lacking of
+
+					// And indicate him of which informations we are lacking of (the color of the corresponding textviews becomes red)
 					if(realName.getText().length() == 0){
 						realName_TV.setTextColor(Color.RED);
 						realName_TV.invalidate();
