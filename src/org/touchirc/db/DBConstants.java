d0ff31c6471819cecd6d10f5870151954a0865db
Bayk <admiquel94@gmail.com>
2013-04-22 22:35:24 +0200

    The Profile Constants are been added.#7

--- src/org/touchirc/db/DBConstants.java
+++ src/org/touchirc/db/DBConstants.java
@@ -1,6 +1,8 @@
 package org.touchirc.db;
 
 import android.provider.BaseColumns;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public interface DBConstants extends BaseColumns {
 	
@@ -12,7 +14,6 @@ public interface DBConstants extends BaseColumns {
     public static final String SERVER_PASSWORD 	 = "password";
     public static final String SERVER_USE_SSL 	 = "useSSL";
     public static final String SERVER_CHARSET 	 = "charset";
-	
     
     public static final String[] SERVER_ALL = {
         SERVER_ID,
@@ -22,6 +23,26 @@ public interface DBConstants extends BaseColumns {
         SERVER_PASSWORD,
         SERVER_USE_SSL,
         SERVER_CHARSET
+    };
+    
+    public static final String PROFILE_TABLE_NAME 		= "profiles";
+	public static final String PROFILE_ID				= "id";
+	public static final String PROFILE_NAME				= "name";
+    public static final String PROFILE_FIRST_NICKNAME 	= "first nickname";
+    public static final String PROFILE_SCD_NICKNAME 	= "second nickname";
+    public static final String PROFILE_THIRD_NICKNAME 	= "third nickname";
+    public static final String PROFILE_USERNAME 		= "username";
+    public static final String PROFILE_REALNAME 		= "realname";
+    public static final String PROFILE_SERVER_LIST 		= "server list";
+    
+    public static final String[] PROFILE_ALL = {
+    	PROFILE_ID,
+    	PROFILE_FIRST_NICKNAME,
+    	PROFILE_SCD_NICKNAME,
+    	PROFILE_THIRD_NICKNAME,
+    	PROFILE_USERNAME,
+    	PROFILE_REALNAME,
+    	PROFILE_SERVER_LIST
     };
     
     
