f6e937fffcf351f2ab5f73a78ebcbc0cf0558cda
t0n3 <leroyer.antoine.tone@gmail.com>
2013-05-26 16:43:10 +0200

    Base for ConversationFragment #4

--- src/org/touchirc/fragments/ConversationFragment.java
+++ src/org/touchirc/fragments/ConversationFragment.java
@@ -1,6 +1,8 @@
 package org.touchirc.fragments;
 
 import org.touchirc.R;
+import org.touchirc.model.Conversation;
+import org.touchirc.model.Message;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,14 +10,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ConversationFragment extends Fragment {
 	
-	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.conversation_fragment, container, false);
-        return rootView;
-    }
+	private TextView textView;
+	private String conversation;
 
+	public ConversationFragment(){
+		this(new Conversation("null")); // Maybe we should find a trick to avoid this :/
+	}
+	
+	public ConversationFragment(Conversation c){
+		this.conversation = getFormattedConversation(c);
+		setRetainInstance(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (savedInstanceState != null)
+			this.conversation = savedInstanceState.getString("conversation");
+		// construct the RelativeLayout
+		RelativeLayout v = new RelativeLayout(getActivity());
+		// Construct the TextView
+		this.textView = new TextView(container.getContext());
+		// Fill the TextView with Conversation
+		this.textView.setText(this.conversation);
+		// Add the TextView to the layout		
+		v.addView(this.textView);
+
+		return v;
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("conversation", this.conversation);
+	}
+
+	public String getFormattedConversation(Conversation c){
+		String s = "";
+		for (Message m : c.getHistory()) {
+			s += "<" + m.getAuthor() + "> " + m.getMessage();
+			s += "\n";
+		}
+		return s;
+	}
 	
 }
