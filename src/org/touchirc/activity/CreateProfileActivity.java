819caa7af4b34824af6d9506c89ff86e8be2f433
t0n3 <leroyer.antoine.tone@gmail.com>
2013-05-31 17:01:04 +0200

    Revert, bad issue number and some mistakes in code
    Please see comments for more informations.
    This reverts commit b6647115bd237a076bc05abaebf618ac43cc6565.

--- src/org/touchirc/activity/CreateProfileActivity.java
+++ src/org/touchirc/activity/CreateProfileActivity.java
@@ -4,13 +4,17 @@ import org.touchirc.R;
 import org.touchirc.TouchIrc;
 import org.touchirc.model.Profile;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -81,25 +85,80 @@ public class CreateProfileActivity extends SherlockActivity{
 		// EditTexts		
 
 		this.profileName_ET = (EditText) findViewById(R.id.editText_profile_name);
-		this.firstNickname_ET = (EditText) findViewById(R.id.editText_first_nick);
-		this.secondNickname_ET = (EditText) findViewById(R.id.editText_second_nick);
-		this.thirdNickname_ET = (EditText) findViewById(R.id.editText_third_nick);
-		this.userName_ET = (EditText) findViewById(R.id.editText_username);
-		this.realName_ET = (EditText) findViewById(R.id.editText_realname);
-		
-		// if started by ExistingProfilesActivity, changing the EditTexts'value
-		if(bundleEdit != null && bundleEdit.containsKey("ProfileId")){
-			// We collect the profile from available profiles list
-			Profile profileToEdit = TouchIrc.getInstance().getAvailableProfiles().valueAt(bundleEdit.getInt("ProfileId"));
-			
-			// And put values in corresponding editText
-			this.profileName_ET.setText(profileToEdit.getProfile_name());
-			this.firstNickname_ET.setText(profileToEdit.getFirstNick());
-			this.secondNickname_ET.setText(profileToEdit.getSecondNick());
-			this.thirdNickname_ET.setText(profileToEdit.getThirdNick());
-			this.userName_ET.setText(profileToEdit.getUsername());
-			this.realName_ET.setText(profileToEdit.getRealname());
+		// if started by ExistingProfilesActivity, changing the EditText
+		if(bundleEdit != null && bundleEdit.containsKey("ProfileName")){
+			this.profileName_ET.setText(bundleEdit.getString("ProfileName"));
 		}
+
+		this.firstNickname_ET = (EditText) findViewById(R.id.editText_first_nick);
+		// if started by ExistingProfilesActivity, changing the EditText
+		if(bundleEdit != null && bundleEdit.containsKey("FirstNickName")){
+			this.firstNickname_ET.setText(bundleEdit.getString("FirstNickName"));
+		}
+
+		this.secondNickname_ET = (EditText) findViewById(R.id.editText_second_nick);
+		// if started by ExistingProfilesActivity, changing the EditText
+		if(bundleEdit != null && bundleEdit.containsKey("ScdNickName") && bundleEdit.getString("ScdNickName").length() > 0){
+			this.secondNickname_ET.setText(bundleEdit.getString("ScdNickName"));
+			this.secondNickname_TV.setTextColor(Color.BLACK); // To highlight the fact that a 2nd nick exists
+		}
+
+		this.thirdNickname_ET = (EditText) findViewById(R.id.editText_third_nick);
+		// if started by ExistingProfilesActivity, changing the EditText
+		if(bundleEdit != null && bundleEdit.containsKey("ThdNickName") && bundleEdit.getString("ThdNickName").length() > 0){
+			this.thirdNickname_ET.setText(bundleEdit.getString("ThdNickName"));
+			this.thirdNickname_TV.setTextColor(Color.BLACK); // To highlight the fact that a 3rd nick exists
+		}
+
+		this.userName_ET = (EditText) findViewById(R.id.editText_username);
+		// if started by ExistingProfilesActivity, changing the EditText
+		if(bundleEdit != null && bundleEdit.containsKey("UserName")){
+			this.userName_ET.setText(bundleEdit.getString("UserName"));
+		}
+
+		this.realName_ET = (EditText) findViewById(R.id.editText_realname);
+		// if started by ExistingProfilesActivity, changing the EditText
+		if(bundleEdit != null && bundleEdit.containsKey("RealName")){
+			this.realName_ET.setText(bundleEdit.getString("RealName"));
+		}
+
+		/**
+		 * Create an OnEditorActionListener Object :
+		 * When the Key "Done" is pressed on the Realname EditText : a Profile is created.
+		 */
+
+		this.realName_ET.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
+				boolean handled = false;
+
+				// the keyboard disappears
+				InputMethodManager mngr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				mngr.hideSoftInputFromWindow(realName_ET.getWindowToken(), 0);
+
+				if (arg1 == EditorInfo.IME_ACTION_DONE && (
+						profileName_ET.getText().length() != 0 && 
+						firstNickname_ET.getText().length() != 0 &&
+						firstNickname_ET.getText().length() <= 9 &&
+						userName_ET.getText().length() != 0 && 
+						realName_ET.getText().length() != 0 &&
+						realName_ET.getText().length() >= 10 &&
+						realName_ET.getText().length() < 20)) {
+
+					addProfile();
+
+					handled = true;					
+					finish(); // close the activity
+				}
+				else{
+					missingInformation();
+				}
+				return handled;
+			}
+
+			
+		});
 	}
 	
 	/**
@@ -205,17 +264,21 @@ public class CreateProfileActivity extends SherlockActivity{
 				userName_ET.getText().toString(),
 				realName_ET.getText().toString());
 
-		Intent i = new Intent(CreateProfileActivity.this, ExistingProfilesActivity.class);
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		Intent i;
 
 		if(bundleEdit != null){
-			
+			/*
 			// Update the Profile in the database
-			TouchIrc.getInstance().updateProfile(bundleEdit.getInt("ProfileId"), prof, getApplicationContext());
+			db.updateProfile(prof, bundleEdit.getString("ProfileName"));
 			Toast.makeText(getApplicationContext(), "The profile : " + prof.getProfile_name() + " has been modified !", Toast.LENGTH_SHORT).show();
 
+			i = new Intent(CreateProfileActivity.this, ExistingProfilesActivity.class);
+			// Use a Bundle to transfer the profile modified
+			b = new Bundle();
+			b.putString("NewNameProfile", prof.getProfile_name());
+			i.putExtra("NewValue", b);
 			startActivity(i);
-			
+			*/
 		}
 		else{
 			// Add the Profile just created into the database
@@ -224,6 +287,7 @@ public class CreateProfileActivity extends SherlockActivity{
 			
 			// We go back to the ExistingProfilesActivity and transmit the new profile
 			if(bundleAddFromMenu != null && bundleAddFromMenu.containsKey("comingFromExistingProfilesActivity")){
+				i = new Intent(CreateProfileActivity.this, ExistingProfilesActivity.class);
 				startActivity(i);
 			}
 		}
