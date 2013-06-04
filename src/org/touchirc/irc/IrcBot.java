ca7f5b46007787d976f45a8840de47daf5470b1a
Boulet <boulet@moiboulet.eu>
2013-06-04 15:12:58 +0200

    Use new system of events from PircBotX #29

--- src/org/touchirc/irc/IrcBot.java
+++ src/org/touchirc/irc/IrcBot.java
@@ -3,7 +3,9 @@ package org.touchirc.irc;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.Event;
 import org.pircbotx.hooks.Listener;
+import org.pircbotx.hooks.events.ConnectEvent;
 import org.pircbotx.hooks.events.JoinEvent;
+import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.events.PartEvent;
 import org.touchirc.model.Conversation;
 import org.touchirc.model.Message;
@@ -20,22 +22,10 @@ public class IrcBot extends PircBotX implements Listener<IrcBot>{
 	public IrcBot(Server server, IrcService service) {
 		this.service = service;
 		this.server = server;
-		this.server.addConversation(new Conversation("#Boulet"));
+		this.getListenerManager().addListener(this);
 	}
 
-	// Automatic answer at "time"
-	public void onMessage(String channel, String sender, String login, String hostname, String message) {
-		if (message.equalsIgnoreCase("time")) {
-			String time = new java.util.Date().toString();
-			sendMessage(channel, sender + ": The time is now " + time);
-		}
-		System.out.println("[IRCBOT] Message recu");
-		this.server.getConversation("#Boulet").addMessage(new Message(message, sender));
-		Intent intent = new Intent();
-		intent.setAction("org.touchirc.irc.newMessage");
-		service.sendBroadcast(intent);
-	}
-	
+		
 	/**
 	 * Set the Nick of the user
 	 * 
@@ -67,29 +57,35 @@ public class IrcBot extends PircBotX implements Listener<IrcBot>{
         this.setVersion(realName);
     }
     
-    
-    protected void onConnect(){
-    	Log.i("[IrbBOT]", "Connected");
-    }
-    
-    protected void onDisconnect(){
-    	Log.i("[IrbBOT]", "Disonnected");
-    }
-
-	@SuppressWarnings("rawtypes")
+    @SuppressWarnings("rawtypes")
 	@Override
 	public void onEvent(Event rawevent) throws Exception {
 		
+    	if(rawevent instanceof MessageEvent){
+    		MessageEvent event = (MessageEvent) rawevent;
+    		this.server.getConversation(event.getChannel().getName()).addMessage(new Message(event.getMessage(), event.getUser().getNick()));
+    		Intent intent = new Intent();
+    		intent.setAction("org.touchirc.irc.newMessage");
+    		service.sendBroadcast(intent);
+    		return;
+    	}
 		if(rawevent instanceof JoinEvent){
 			JoinEvent event = (JoinEvent) rawevent;
-			if(event.getUser().equals(event.getBot().getUserBot()) && this.server.getConversation(event.getChannel().getName()) == null)
+			if(event.getUser().equals(event.getBot().getUserBot()) && this.server.getConversation(event.getChannel().getName()) == null){
 				this.server.addConversation(new Conversation(event.getChannel().getName()));
+				System.out.println("JoinEvent "+event.getChannel().getName());
+			}
 			return;
 		}
 		if(rawevent instanceof PartEvent){
 			PartEvent event = (PartEvent) rawevent;
 			if(event.getUser().equals(event.getBot().getUserBot()) && this.server.getConversation(event.getChannel().getName()) != null)
 				this.server.removeConversation(event.getChannel().getName());
+			return;
+		}
+		if(rawevent instanceof ConnectEvent){
+			ConnectEvent event = (ConnectEvent) rawevent;
+			Log.i("[IrcBot - " + event.getBot().getName() + "]", "Connected");
 			return;
 		}
 		
